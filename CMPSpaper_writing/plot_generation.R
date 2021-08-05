1997efa940aaf96cf1265652556c884d18e5f58b
willju-wangqian <wju@iastate.edu>
2021-08-04 20:43:31 -0500

    added pipeline code for hamby44

--- CMPSpaper_writing/plot_generation.R
+++ CMPSpaper_writing/plot_generation.R
@@ -76,8 +76,214 @@ dframe %>% ggplot(aes(x = landB, y = landA, fill = cmps_score)) + geom_tile() +
 
 
 #######################
-# check 1-4 vs 2-5, segment 8
+# check 1-4 vs 2-5, segment 8 -> expected NA, got INF
+
+#########################
+# generate plots for show & tell
+CMPS_hamby252_results <- readRDS("~/Research/CMPSpaper/CMPSpaper_writing/data/h252-seg_length.rds")
+com.title252 <- expression(paste(
+  "Hamby 252 - ", CMPS[max], ", ", bar(CMPS)[max], " and ", bar(CMPS)[diff] ," Distribution"
+))
 
 
+for (i in 1:length(CMPS_hamby252_results$signame)) {
+  CMPS_hamby252_results$cmps.table[[i]] -> hamby252.cmps
+  
+  hamby252.cmps <- hamby252.cmps %>% mutate(
+    cmps.max_scaled = cmps.table.m %>% purrr::map_dbl(
+      .f = function(t) {
+        max(t$cmps_score_scaled, na.rm = TRUE)
+      }
+    ),
+    cmps.maxbar_scaled = cmps.table.m %>% purrr::map_dbl(
+      .f = function(t) {
+        landidx1 <-
+          t$land1 %>% as.character() %>% str_sub(-1, -1) %>% as.numeric()
+        landidx2 <-
+          t$land2 %>% as.character() %>% str_sub(-1, -1) %>% as.numeric()
+        compute_average_scores(landidx1, landidx2, t$cmps_score_scaled, addNA = TRUE) %>% max()
+      }
+    ),
+    cmps.max = cmps.table.m %>% purrr::map_dbl(
+      .f = function(t) {
+        max(t$cmps_score, na.rm = TRUE)
+      }
+    ),
+    cmps.maxbar = cmps.table.m %>% purrr::map_dbl(
+      .f = function(t) {
+        landidx1 <-
+          t$land1 %>% as.character() %>% str_sub(-1, -1) %>% as.numeric()
+        landidx2 <-
+          t$land2 %>% as.character() %>% str_sub(-1, -1) %>% as.numeric()
+        compute_average_scores(landidx1, landidx2, t$cmps_score, addNA = TRUE) %>% max()
+      }
+    )
+  )
+  
+  CMPS_hamby252_results$cmps.table[[i]] <- hamby252.cmps
+}
 
+# plot
+CMPS_hamby252_results$plot_scaled <- list()
+CMPS_hamby252_results$plot <- list()
+
+for (i in 1:length(CMPS_hamby252_results$signame)) {
+  hamby252.cmps <- CMPS_hamby252_results$cmps.table[[i]]
+  
+  hamby252.plot.list <- list()
+  titlee <- CMPS_hamby252_results$titlee[[i]]
+  
+  hamby252.plot.list[[1]] <- hamby252.cmps %>% ggplot() +
+    geom_histogram(aes(x = cmps.max_scaled,
+                       fill = as.factor(type_truth)), binwidth = 0.04) +
+    labs(
+      fill = "Comparison Type",
+      x = expression(CMPS^"*"[max]),
+      # title = expression(paste("Hamby252 - ", CMPS[max], " Distribution")),
+      subtitle = titlee
+    ) +
+    scale_x_continuous(breaks = pretty(seq(0, 1, 0.05), n=10)) +
+    theme_bw() +
+    theme(panel.grid.minor = element_blank()) +
+    font("x.text", size = 6) +
+    scale_fill_manual(values=c("darkorange", "darkgrey"))
+  
+  hamby252.plot.list[[2]] <- hamby252.cmps %>% ggplot() +
+    geom_histogram(aes(x = cmps.maxbar_scaled,
+                       fill = as.factor(type_truth)), binwidth = 0.04) +
+    labs(
+      x = expression(bar(CMPS^"*")[max]),
+      fill = "Comparison Type",
+      # title = expression(paste(
+      #   "Hamby252 - ", bar(CMPS)[max], " Distribution"
+      # )),
+      subtitle = titlee
+    ) +
+    scale_x_continuous(breaks = pretty(seq(0, 1, 0.05), n=10)) +
+    theme_bw() +
+    theme(panel.grid.minor = element_blank()) +
+    scale_fill_manual(values=c("darkorange", "darkgrey"))
+  
+  hamby252.plot.list[[3]] <- hamby252.cmps %>% ggplot() +
+    geom_histogram(aes(x = cmps.diff_scaled,
+                       fill = as.factor(type_truth)), binwidth = 0.05) +
+    labs(
+      x = expression(bar(CMPS^"*")[diff]),
+      fill = "Comparison Type",
+      # title = expression(paste(
+      #   "Hamby252 - ", bar(CMPS)[max], " Distribution"
+      # )),
+      subtitle = titlee
+    ) +
+    scale_x_continuous(breaks = pretty(seq(0, 1, 0.05), n=10)) +
+    theme_bw() +
+    theme(panel.grid.minor = element_blank()) +
+    scale_fill_manual(values=c("darkorange", "darkgrey"))
+  
+  plot <- ggarrange(plotlist = hamby252.plot.list,
+                    nrow = 1,
+                    ncol = 3,
+                    common.legend = TRUE, legend = "bottom")
+  plot <- annotate_figure(plot, 
+                          top = text_grob(com.title252))
+  CMPS_hamby252_results$plot_scaled[[i]] <- plot
+}
+
+for (i in 1:length(CMPS_hamby252_results$signame)) {
+  hamby252.cmps <- CMPS_hamby252_results$cmps.table[[i]]
+  
+  hamby252.plot.list <- list()
+  titlee <- CMPS_hamby252_results$titlee[[i]]
+  
+  hamby252.plot.list[[1]] <- hamby252.cmps %>% ggplot() +
+    geom_histogram(aes(x = cmps.max,
+                       fill = as.factor(type_truth)), binwidth = 1) +
+    labs(
+      fill = "Comparison Type",
+      x = expression(CMPS[max]),
+      # title = expression(paste("Hamby252 - ", CMPS[max], " Distribution")),
+      subtitle = titlee
+    ) +
+    scale_x_continuous(breaks = seq(0, 27, 1)) +
+    theme_bw() +
+    theme(panel.grid.minor = element_blank()) +
+    font("x.text", size = 6) +
+    scale_fill_manual(values=c("darkorange", "darkgrey"))
+  
+  hamby252.plot.list[[2]] <- hamby252.cmps %>% ggplot() +
+    geom_histogram(aes(x = cmps.maxbar,
+                       fill = as.factor(type_truth)), binwidth = 1) +
+    labs(
+      x = expression(bar(CMPS)[max]),
+      fill = "Comparison Type",
+      # title = expression(paste(
+      #   "Hamby252 - ", bar(CMPS)[max], " Distribution"
+      # )),
+      subtitle = titlee
+    ) +
+    scale_x_continuous(breaks = seq(0, 24, 1)) +
+    theme_bw() +
+    theme(panel.grid.minor = element_blank()) +
+    scale_fill_manual(values=c("darkorange", "darkgrey"))
+  
+  hamby252.plot.list[[3]] <- hamby252.cmps %>% ggplot() +
+    geom_histogram(aes(x = cmps.diff,
+                       fill = as.factor(type_truth)), binwidth = 1) +
+    labs(
+      x = expression(bar(CMPS)[diff]),
+      fill = "Comparison Type",
+      # title = expression(paste(
+      #   "Hamby252 - ", bar(CMPS)[max], " Distribution"
+      # )),
+      subtitle = titlee
+    ) +
+    scale_x_continuous(breaks = seq(0, 24, 1)) +
+    theme_bw() +
+    theme(panel.grid.minor = element_blank()) +
+    scale_fill_manual(values=c("darkorange", "darkgrey"))
+  
+  plot <- ggarrange(plotlist = hamby252.plot.list,
+                    nrow = 1,
+                    ncol = 3,
+                    common.legend = TRUE, legend = "bottom")
+  plot <- annotate_figure(plot, 
+                          top = text_grob(com.title252))
+  CMPS_hamby252_results$plot[[i]] <- plot
+}
+
+CMPS_hamby252_results$plot[[1]]
+CMPS_hamby252_results$plot[[4]]
+
+CMPS_hamby252_results$plot_scaled[[1]]
+CMPS_hamby252_results$plot_scaled[[4]]
+
+##########
+# variance ratio
+table.cmps.diff <- lapply(CMPS_hamby252_results$cmps.table, function(tt) {
+  r1 <- compute_var_ratio_anova(tt$cmps.diff, tt$type_truth, MS=FALSE)
+  r2 <- compute_var_ratio_anova(tt$cmps.diff_scaled, tt$type_truth, MS=FALSE)
+  r3 <- compute_var_ratio_anova(tt$cmps.max, tt$type_truth, MS=FALSE)
+  r4 <- compute_var_ratio_anova(tt$cmps.max_scaled, tt$type_truth, MS=FALSE)
+  r5 <- compute_var_ratio_anova(tt$cmps.maxbar, tt$type_truth, MS=FALSE)
+  r6 <- compute_var_ratio_anova(tt$cmps.maxbar_scaled, tt$type_truth, MS=FALSE)
+  c(r1, r2, r3, r4, r5, r6)
+}) %>% do.call(rbind, .) %>% as.data.frame()
+
+colnames(table.cmps.diff) <- c("cmps.diff", "cmps.diff_scaled",
+                               "cmps.max", "cmps.max_scaled",
+                               "cmps.maxbar", "cmps.maxbar_scaled")
+table.cmps.diff$seg_length <- unlist(CMPS_hamby252_results$seg_length)
+
+longer.tt <- table.cmps.diff %>% pivot_longer(cols = cmps.diff:cmps.maxbar_scaled,
+                                 names_to = "score_name")
+longer.tt <- longer.tt %>% mutate(
+  scaled = str_detect(score_name, "scaled"),
+  metric = str_remove(score_name, "_scaled")
+)
+
+longer.tt %>%
+  ggplot(aes(x = seg_length, y = value)) +
+  geom_line(aes(color = scaled, linetype = metric)) +
+  scale_linetype_manual(values = c("solid", "dotted", "dashed")) +
+  theme_bw()
 
--- CMPSpaper_writing/results_generation_param_44.R
+++ CMPSpaper_writing/results_generation_param_44.R
@@ -1,12 +1,3 @@
-##########################
-# Note: This R script uses rds files in reproducible/bullet_signatures_etc/
-#       These rds files contain processed bullet signatures used in the paper
-#       The original data contains x3p objects of large size, so they are not
-#       uploaded to the Github repo
-##########################
-
-
-## Load the required packages
 library(tidyverse)
 library(bulletxtrctr)
 library(x3ptools)
@@ -14,77 +5,90 @@ library(CMPS)
 library(ggpubr)
 library(parallel)
 
-#### code used to load the original data; commented out
-# b44.full <- 
-#   readRDS("~/Research/CMPSpaper/preconsideration/hamby44_manual5.rds")
+source("CMPSpaper_writing/func_collection.R")
 
-## Data Processing
+###########
+# from the x3p data, re-run CMPS results for hamby 44
+# hamby44_results <-
+#   readRDS("C:/Research/Bullet/Bullet_Rcode/hamby44_results.rds")
 
-#### load the data from reproducible/bullet_signatures_etc/
-b44 <- read_rds("./reproducible/bullet_signatures_etc/BulletSignatures44.rds")
+# b44.full <-
+#   readRDS("C:/Research/Bullet/Bullet_Rcode/hamby44/hamby44_manual5.rds")
+b44.full <- 
+  readRDS("~/Research/CMPSpaper/preconsideration/hamby44_manual5.rds")
 
-#### obtain all comparisons
-bulletid.tb <- b44 %>% select(scan_id)
+# rds file that stores indices of damaged scans
+# idf.idx <-
+#   readRDS("C:/Research/Bullet/Bullet_Rcode/hamby44/idfidx.rds")
+# idf.idx <- 
+#   readRDS("~/Research/CMPSpaper/preconsideration/idfidx.rds")
+# 
+# idf.scan_id <- b44.full %>% slice(idf.idx) %>% .$scan_id
+idf.idx <- c(23,35,39,41,102,126,149)
+idf.scan_id <- b44.full %>% slice(idf.idx) %>% .$scan_id
+
+# generate bullet_id
+bulletid.tb <- b44.full %>% select(scan_id)
 bulletid.tb <- bulletid.tb %>% mutate(
   barrel_id = sapply(strsplit(scan_id, "-"), "[[", 1),
   bullet = sapply(strsplit(scan_id, "-"), "[[", 2),
   land_id = sapply(strsplit(scan_id, "-"), "[[", 3),
   bullet_id = paste(barrel_id, bullet)
 )
+
+
+# remove x3p files
+b44 <- b44.full %>% select(-x3p,-crosscut,-grooves,-ccdata)
+
+# get all comparisons
 b44$bullet_id <- bulletid.tb %>% pull(bullet_id)
 b.cb <- unique(b44$bullet_id) %>% utils::combn(m = 2)
 p <- 1:595
 
-#### import indices of damaged scans
-###### These indices are identified by using a Shiny App called bulletinspectR
-idf.idx <- c(23,35,39,41,102,126,149)
-idf.scan_id <- b44.full %>% slice(idf.idx) %>% .$scan_id
-
-#### include the ground truth for Hamby44
+# get ground-truth
 key.table <- tibble(
-  bullet.id = c("K",
-                "O",
-                "L",
-                "P",
-                "J",
-                "H",
-                "I",
-                "Y",
-                "G",
-                "E",
-                "U",
-                "X",
-                "F",
-                "T",
-                "S"),
+  bullet.id = c(
+    "K",
+    "O",
+    "L",
+    "P",
+    "J",
+    "H",
+    "I",
+    "Y",
+    "G",
+    "E",
+    "U",
+    "X",
+    "F",
+    "T",
+    "S"
+  ),
   barrel.id = c(1, 2, 3, 4, 4, 5, 5, 6, 6, 7, 7, 7, 8, 9, 10)
 )
 
-tmp.table.44 <- tibble(bullet1 = b.cb[1,],
-                       bullet2 = b.cb[2,])
+tmp.table.44 <- tibble(bullet1 = b.cb[1, ],
+                       bullet2 = b.cb[2, ])
 
 tmp.table.44$b1t <-
   tmp.table.44$bullet1 %>% str_extract("'[A-Z]'") %>% str_extract("[A-Z]")
 tmp.table.44$b2t <-
   tmp.table.44$bullet2 %>% str_extract("'[A-Z]'") %>% str_extract("[A-Z]")
 
-tmp.table.44$b1b <- tmp.table.44 %>% 
-  left_join(key.table, by = c("b1t" = "bullet.id")) %>% pull(barrel.id)
-tmp.table.44$b2b <- tmp.table.44 %>% 
-  left_join(key.table, by = c("b2t" = "bullet.id")) %>% pull(barrel.id)
+tmp.table.44$b1b <-
+  tmp.table.44 %>% left_join(key.table, by = c("b1t" = "bullet.id")) %>% pull(barrel.id)
+tmp.table.44$b2b <-
+  tmp.table.44 %>% left_join(key.table, by = c("b2t" = "bullet.id")) %>% pull(barrel.id)
 
-####### KM - come from the same barrel; KNM - otherwise
 tmp.table.44 <- tmp.table.44 %>% mutate(
   b1b = if_else(
     is.na(b1b),
-    bullet1 %>% str_extract("Barrel \\d+") %>% 
-      str_extract("\\d+") %>% as.numeric(),
+    bullet1 %>% str_extract("Barrel \\d+") %>% str_extract("\\d+") %>% as.numeric(),
     b1b
   ),
   b2b = if_else(
     is.na(b2b),
-    bullet2 %>% str_extract("Barrel \\d+") %>% 
-      str_extract("\\d+") %>% as.numeric(),
+    bullet2 %>% str_extract("Barrel \\d+") %>% str_extract("\\d+") %>% as.numeric(),
     b2b
   ),
   type_truth = if_else(b1b == b2b, "KM", "KNM")
@@ -105,30 +109,29 @@ tmp.table.44 <- tmp.table.44 %>% mutate(
   )
 )
 
-## set up parameters
-N <- 3 # the number of different sets of parameters
-CMPS_hamby44_results <- list() # a container for everything
 
-#### setup span1
-CMPS_hamby44_results$span1 <- list(0.25, 0.25, 0.75)
-
-#### setup signature name
+N <- 9
+CMPS_hamby44_results <- list()
+CMPS_hamby44_results$span1 <- as.list(rep(0.25, N))
 CMPS_hamby44_results$signame <-
-  list("sigs25_531", "sigs25_1061", "sigs75_531")
-
-#### setup neapks.set
+  # list("n5", "n51", "n52", "n531", "n10-62", "n6421", 
+  #      "n10-742", "n10-6421")
+  list("s30", "s45", "s61", "s90", "s122", "s150", "s180", "s210", "s61-ol")
 CMPS_hamby44_results$npeaks.set <-
-  list(c(5, 3, 1),
-       c(10, 6, 1),
-       c(5, 3, 1))
-
-#### setup seg_length
-CMPS_hamby44_results$seg_length <- as.list(rep(61, 3))
-
-#### setup Tx
-CMPS_hamby44_results$Tx <- as.list(rep(30, 3))
-
-#### create a plot title and a file name for each set of parameters
+  # list(c(5),
+  #      c(5,1),
+  #      c(5,2),
+  #      c(5,3,1),
+  #      c(10,6,2),
+  #      c(6,4,2,1),
+  #      c(10,7,4,2),
+  #      c(10,6,4,2,1))
+  lapply(1:N, function(t) c(5,3,1))
+CMPS_hamby44_results$seg_length <- 
+  as.list(c(30, 45, 61, 90, 122, 150, 180, 210, 62))
+CMPS_hamby44_results$outlength <- list()
+CMPS_hamby44_results$outlength[[N]] <- c(62, 122, 183)
+CMPS_hamby44_results$Tx <- as.list(rep(30, N))
 CMPS_hamby44_results$titlee <- list()
 CMPS_hamby44_results$filename <- list()
 for (i in 1:N) {
@@ -155,36 +158,32 @@ for (i in 1:N) {
       sep = "_"
     )
 }
-
-#### container for the cmps results and corresponding plots
 CMPS_hamby44_results$cmps.table <- list()
 CMPS_hamby44_results$plot <- list()
 
-#### a global title for Hamby44 plots
 com.title44 <- expression(paste(
   "Hamby 44 - ", CMPS[max], " and ", bar(CMPS)[max], " Distribution"
 ))
 
-## Compute CMPS scores
 
-#### for each set of parameters, compute the CMPS scores
+# start of the loop!
 for (i in 1:N) {
+  # i <- 1
   
-  ###### code for generating bullet signatures; commented out
-  # b44[[CMPS_hamby44_results$signame[[i]]]] <- purrr::map2(
-  #   .x = b44.full$ccdata,
-  #   .y = b44.full$grooves,
-  #   .f = function(x, y) {
-  #     cc_get_signature(
-  #       ccdata = x,
-  #       grooves = y,
-  #       span1 = CMPS_hamby44_results$span1[[i]],
-  #       span2 = 0.03
-  #     )
-  #   }
-  # )
+  b44[[CMPS_hamby44_results$signame[[i]]]] <- purrr::map2(
+    .x = b44.full$ccdata,
+    .y = b44.full$grooves,
+    .f = function(x, y) {
+      cc_get_signature(
+        ccdata = x,
+        grooves = y,
+        span1 = CMPS_hamby44_results$span1[[i]],
+        span2 = 0.03
+      )
+    }
+  )
   
-  #### remove outliers in bullet signatures
+  # process of removing outliers
   tt <-
     lapply(b44[[CMPS_hamby44_results$signame[[i]]]], function(x) {
       x$sig
@@ -204,8 +203,7 @@ for (i in 1:N) {
     }
   ))
   
-  
-  #### setup the parallel computing (code works for Windows machines)
+  ## parallel setup
   cl <- makeCluster(7)
   
   par.setup <- parLapply(cl, 1:length(cl),
@@ -221,11 +219,11 @@ for (i in 1:N) {
                   "CMPS_hamby44_results", "i"),
                 envir = globalenv())
   
-  #### start the parallel computing
+  # start the parallel computing
   system.time({
     tmp.44.list <- parLapply(cl, p, function(cb.idx) {
+      # cat(" ############# \n", "start of ", cb.idx, "\n")
       
-      # identify two bullets with their IDs
       tmp.lands <-
         c(
           b44 %>% filter(bullet_id %in% b.cb[, cb.idx][1]) %>% pull(scan_id),
@@ -235,7 +233,7 @@ for (i in 1:N) {
         data.frame(expand.grid(land1 = tmp.lands[1:6], land2 = tmp.lands[7:12]),
                    stringsAsFactors = FALSE)
       
-      # compute CMPS scores for each signature comparison
+      # cat(cb.idx, "- 1; ")
       tmp.comp$cmps <-
         purrr::map2(
           .x = tmp.comp$land1,
@@ -250,12 +248,14 @@ for (i in 1:N) {
               npeaks.set = CMPS_hamby44_results$npeaks.set[[i]],
               seg_length = CMPS_hamby44_results$seg_length[[i]],
               Tx = CMPS_hamby44_results$Tx[[i]],
-              include = "nseg"
+              include = "nseg",
+              outlength = CMPS_hamby44_results$outlength[[i]] # new added
             )
           }
         )
       
-      # extract CMPS scores
+      # cat(cb.idx, "- 3; ")
+      
       tmp.comp <- tmp.comp %>%
         mutate(
           cmps_score = sapply(tmp.comp$cmps, function(x)
@@ -264,9 +264,14 @@ for (i in 1:N) {
             x$nseg)
         )
       
-      # summarize results and return
+      # cat(cb.idx, "- 4;\n")
+      
       cmps.table <-
         tmp.comp %>% select(land1, land2, cmps_score, cmps_nseg)
+      # cp2 <- tmp.comp %>% select(land1, land2, cmps_score, cmps_nseg)
+      
+      # cat(" end of ", cb.idx, "\n", "############# \n")
+      
       tibble(
         bullet1 = b.cb[, cb.idx][1],
         bullet2 = b.cb[, cb.idx][2],
@@ -277,60 +282,46 @@ for (i in 1:N) {
   
   # user  system elapsed
   # 0.02    0.00  147.06
+  
+  
   hamby44.cmps <- do.call(rbind, tmp.44.list)
   
   stopCluster(cl)
   
-  # compute CMPS_{max} and \bar{CMPS_{max}} for the bullet comparison
-  # before removing damaged scans
-  hamby44.cmps <- hamby44.cmps %>% mutate(
-    cmps.max = cmps.table %>% purrr::map_dbl(
-      .f = function(t) {
-        max(t$cmps_score)
-      }
-    ),
-    cmps.maxbar = cmps.table %>% purrr::map_dbl(
-      .f = function(t) {
-        landidx1 <-
-          t$land1 %>% as.character() %>% str_sub(-1, -1) %>% as.numeric()
-        landidx2 <-
-          t$land2 %>% as.character() %>% str_sub(-1, -1) %>% as.numeric()
-        compute_average_scores(landidx1, landidx2, t$cmps_score) %>% max()
-      }
-    )
-  )
-  
-  # set NAs for tank-rashed data
   hamby44.cmps$cmps.table.m <-
     lapply(hamby44.cmps$cmps.table, function(tt) {
       tt.idx <- tt %>% rowid_to_column %>%
         filter(land1 %in% idf.scan_id |
                  land2 %in% idf.scan_id) %>% pull(rowid)
       tt[tt.idx, "cmps_score"] <- NA
+      tt$cmps_score_scaled <- tt$cmps_score / tt$cmps_nseg
       tt
     })
   
-  # after removing tank-rashed data, recompute CMPS_{max} and \bar{CMPS_{max}}
+  
+  # after removing tank-rashed data
   hamby44.cmps <- hamby44.cmps %>% mutate(
-    cmps.max.m = cmps.table.m %>% purrr::map_dbl(
-      .f = function(t) {
-        max(t$cmps_score, na.rm = TRUE)
-      }
-    ),
-    cmps.maxbar.m = cmps.table.m %>% purrr::map_dbl(
+    cmps.diff = cmps.table.m %>% purrr::map_dbl(
       .f = function(t) {
         landidx1 <-
           t$land1 %>% as.character() %>% str_sub(-1, -1) %>% as.numeric()
         landidx2 <-
           t$land2 %>% as.character() %>% str_sub(-1, -1) %>% as.numeric()
-        compute_average_scores(
-          landidx1, landidx2, t$cmps_score, addNA = TRUE
-        ) %>% max()
+        compute_avgscore_denoise(landidx1, landidx2, t$cmps_score, addNA = TRUE)
+      }
+    ),
+    cmps.diff_scaled = cmps.table.m %>% purrr::map_dbl(
+      .f = function(t) {
+        landidx1 <-
+          t$land1 %>% as.character() %>% str_sub(-1, -1) %>% as.numeric()
+        landidx2 <-
+          t$land2 %>% as.character() %>% str_sub(-1, -1) %>% as.numeric()
+        compute_avgscore_denoise(landidx1, landidx2, t$cmps_score_scaled, addNA = TRUE)
       }
     )
   )
   
-  # add ground truth and type
+  # add groundtruth
   hamby44.cmps$type_truth <- tmp.table.44$type_truth
   hamby44.cmps$type <- tmp.table.44$type
   
@@ -338,50 +329,50 @@ for (i in 1:N) {
   CMPS_hamby44_results$cmps.table[[i]] <- hamby44.cmps
   
 }
+saveRDS(CMPS_hamby44_results, "./CMPSpaper_writing/data/h44-seg_length.rds")
 
-#### code used to save results as csv files used in the paper; commented out
-# data_path <- "~/Research/CMPSpaper/CMPSpaper_writing/data/"
-# for(i in 1:N){
-#   write.csv(
-#     CMPS_hamby44_results$cmps.table[[i]] %>% select(-cmps.table, -cmps.table.m) %>% as.data.frame(),
-#     file = paste(data_path, CMPS_hamby44_results$filename[[i]], ".csv", sep = ""))
-# }
 
-## Generate plots
+
+#################################################
+# generate plots
 for (i in 1:N) {
   hamby44.cmps <- CMPS_hamby44_results$cmps.table[[i]]
   
   hamby44.plot.list <- list()
   titlee <- CMPS_hamby44_results$titlee[[i]]
   
-  # generate plot for CMPS_{max}
   hamby44.plot.list[[1]] <- hamby44.cmps %>% ggplot() +
-    geom_histogram(aes(x = cmps.max.m,
+    geom_histogram(aes(x = cmps.diff,
                        fill = as.factor(type_truth)), binwidth = 1) +
     labs(
+      x = expression(bar(CMPS)[diff]),
       fill = "Comparison Type",
-      x = expression(CMPS[max]),
-      subtitle = titlee
-    ) +
-    scale_x_continuous(breaks = seq(0, 27, 1)) +
-    theme_bw() +
-    theme(panel.grid.minor = element_blank()) +
-    font("x.text", size = 6)
-  
-  # generate plot for \bar{CMPS_{max}}
-  hamby44.plot.list[[2]] <- hamby44.cmps %>% ggplot() +
-    geom_histogram(aes(x = cmps.maxbar.m,
-                       fill = as.factor(type_truth)), binwidth = 1) +
-    labs(
-      x = expression(bar(CMPS)[max]),
-      fill = "Comparison Type",
+      # title = expression(paste(
+      #   "Hamby44 - ", bar(CMPS)[max], " Distribution"
+      # )),
       subtitle = titlee
     ) +
     scale_x_continuous(breaks = seq(0, 24, 1)) +
     theme_bw() +
-    theme(panel.grid.minor = element_blank())
+    theme(panel.grid.minor = element_blank()) +
+    scale_fill_manual(values=c("darkorange", "darkgrey"))
   
-  # combine the two plots
+  hamby44.plot.list[[2]] <- hamby44.cmps %>% ggplot() +
+    geom_histogram(aes(x = cmps.diff_scaled,
+                       fill = as.factor(type_truth)), binwidth = 0.04) +
+    labs(
+      x = expression(bar(CMPS^"*")[diff]),
+      fill = "Comparison Type",
+      # title = expression(paste(
+      #   "Hamby44 - ", bar(CMPS)[max], " Distribution"
+      # )),
+      subtitle = titlee
+    ) +
+    scale_x_continuous(breaks = seq(0, 1, 0.05)) +
+    theme_bw() +
+    theme(panel.grid.minor = element_blank()) +
+    scale_fill_manual(values=c("darkorange", "darkgrey"))
+  
   plot <- ggarrange(plotlist = hamby44.plot.list,
                     nrow = 1,
                     ncol = 2,
@@ -390,3 +381,53 @@ for (i in 1:N) {
                           top = text_grob(com.title44))
   CMPS_hamby44_results$plot[[i]] <- plot
 }
+
+# saveRDS(CMPS_hamby44_results, "./CMPSpaper_writing/data/h44-neapk.rds")
+
+
+CMPS_hamby44_results <- readRDS("~/Research/CMPSpaper/CMPSpaper_writing/data/h44-neapk.rds")
+CMPS_hamby44_results$plot[[9]]
+CMPS_hamby44_results$plot[[3]]
+CMPS_hamby44_results$plot[[5]]
+
+# compute variance ratio and generate plots
+table.cmps.diff.44 <- lapply(CMPS_hamby44_results$cmps.table, function(tt) {
+  r1 <- compute_var_ratio_anova(tt$cmps.diff, tt$type_truth, MS=FALSE)
+  r2 <- compute_var_ratio_anova(tt$cmps.diff_scaled, tt$type_truth, MS=FALSE)
+  c(r1, r2)
+}) %>% do.call(rbind, .) %>% as.data.frame()
+
+colnames(table.cmps.diff.44) <- c("cmps.diff", "cmps.diff_scaled")
+
+# npeak
+table.cmps.diff.44$npeak <- unlist(CMPS_hamby44_results$signame)
+table.cmps.diff.44 %>% 
+  mutate(
+    npeak = factor(
+      npeak,
+      levels = npeak[order(cmps.diff_scaled, decreasing = TRUE)])
+  ) %>% 
+  pivot_longer(cols = c(cmps.diff, cmps.diff_scaled),
+               names_to = "score") %>% 
+  ggplot(aes(x=npeak, y=value)) +
+  geom_bar(aes(fill = score),position = "dodge", stat = "identity")
+
+# seg_length
+table.cmps.diff.44$seg_length <- unlist(CMPS_hamby44_results$seg_length)
+
+table.cmps.diff.44 %>%
+  pivot_longer(cols = c(cmps.diff, cmps.diff_scaled),
+               names_to = "score") %>%
+  ggplot(aes(x = seg_length, y = value)) +
+  geom_line(aes(color = score))
+
+order(table.cmps.diff.44$cmps.diff_scaled, decreasing = TRUE)
+table.cmps.diff.44$cmps.diff
+table.cmps.diff.44$cmps.diff_scaled
+
+
+
+
+
+
+
